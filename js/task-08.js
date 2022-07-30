e7e24c3477b3b22f7b8f5caf6b0fd5bca42c9af1
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2022-07-30 12:25:16 +0300

    8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", formSubmit);
+
+function formSubmit(event) {
+    event.preventDefault();
+    const{
+        elements: {email, password}
+    } = event.currentTarget;
+
+        if(email.value === "" || password.value === ""){
+       return alert("Заповніть всі поля");
+    }
+
+    console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
