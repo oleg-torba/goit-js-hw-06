e85e02b55dce9bc958584b1c3e7080cb888d7728
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2022-08-02 21:41:53 +0300

    update 06

--- js/task-06.js
+++ js/task-06.js
@@ -3,8 +3,10 @@ const getTargetLength = Number(validation.getAttribute("data-length"));
 
 validation.addEventListener(`blur`, function () {
   if (validation.value.length === getTargetLength) {
+    validation.classList.remove(`invalid`);
     validation.classList.add(`valid`);
   } else {
+    validation.classList.remove(`valid`)
     validation.classList.add(`invalid`);
   }
 });
